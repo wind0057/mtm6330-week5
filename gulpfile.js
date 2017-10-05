77575b4de6073badfe7dbc01607822d5bc1f3598
Robert McKenney <robert@mckenney.ca>
2017-10-05 15:09:08 -0400

    Add gulp and gulp-sass

---
+++ gulpfile.js
@@ -0,0 +1,20 @@
+// Declare global scope variables.
+const gulp = require('gulp')      // load the gulp library
+const sass = require('gulp-sass') // load the gulp-sass compiler library
+const sassPath = 'scss/**/*.scss' // the folder(s) with our Sass source files
+
+// Define a new task called 'sass' that we can call to compile Sass to CSS
+gulp.task('sass', function () {
+  return gulp
+    .src(sassPath)          // where to find the Sass source files (use our variable)
+    .pipe(sass())           // forward those files to the compiler
+    .pipe(gulp.dest('css')) // where to output the comipled CSS files
+})
+
+// Define a new default task (so we can just call 'gulp' on the command line)
+// to automatically compile when we save changes to our Sass files
+gulp.task('default', function () {
+  gulp.watch(
+    sassPath, // which files to watch for changes (use our variable)
+    ['sass']) // an array of tasks to run when changes are detected.
+})
