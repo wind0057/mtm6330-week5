ff14e7652edd184c568cfe795cd443d29c7db9a6
Scott W <wind0057@algonquinlive.com>
2017-10-06 11:27:54 -0400

    Add postCSS to Gulp compiler

--- gulpfile.js
+++ gulpfile.js
@@ -2,12 +2,22 @@
 const gulp = require('gulp')      // load the gulp library
 const sass = require('gulp-sass') // load the gulp-sass compiler library
 const sassPath = 'scss/**/*.scss' // the folder(s) with our Sass source files
+const postcss = require('gulp-postcss')
+const autoprefixer = require('autoprefixer')
+const cssnano = require('cssnano')
 
 // Define a new task called 'sass' that we can call to compile Sass to CSS
 gulp.task('sass', function () {
+
+  const plugins = [
+    autoprefixer({ browsers: ['last 2 versions']}),
+    cssnano()
+  ]
+
   return gulp
     .src(sassPath)          // where to find the Sass source files (use our variable)
     .pipe(sass())           // forward those files to the compiler
+    .pipe(postcss(plugins)) // apply the postcss plugin
     .pipe(gulp.dest('css')) // where to output the comipled CSS files
 })
 
